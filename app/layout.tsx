52f45570973f34f51444a1f03ba3970948579638
Sahil Singh Tomar <sahilsinghtomar14435@gmail.com>
2024-10-13 14:15:34 +0530

    fixed layout bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import BreakpointIndicator from "@/components/breakpoint-indicator";
 
 export const metadata: Metadata = {
-  title: 'Visey',
-  description: 'promote your startup and small business and grow more with visey',
+  title: "Visey",
+  description:
+    "promote your startup and small business and grow more with visey",
 };
 
 export default function RootLayout({
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-inter antialiased text-base-black`}>
+        <BreakpointIndicator />
         <div className="flex flex-col min-h-screen w-full pb-24 md:pb-0">
           {children}
         </div>
