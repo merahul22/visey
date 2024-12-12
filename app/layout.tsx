0be26063c9b62c58e6dea7bdc0a44bd949e473e0
Rahul chourasiya <130483145+merahul22@users.noreply.github.com>
2024-12-12 18:01:09 +0000

    redeploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import BreakpointIndicator from '@/components/breakpoint-indicator';
+// import BreakpointIndicator from '@/components/breakpoint-indicator';
 import Provider from '@/components/Provider';
 import HelpAndSupport from '@/components/HelpAndSupport';
 import { Toaster } from "@/components/ui/sonner"
