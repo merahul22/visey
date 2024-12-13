d5937c453fb90c6cede956dfbeb66b00561046f1
Sahil Singh Tomar <sahilsinghtomar14435@gmail.com>
2024-12-13 22:55:41 +0530

    made tnc route public

--- routes.ts
+++ routes.ts
@@ -1,4 +1,4 @@
-export const publicRoutes = ['/', '/demo-account-type', '/about', '/pricing', '/contact-us', '/partner-with-us', '/blogs'];
+export const publicRoutes = ['/', '/demo-account-type', '/about', '/pricing', '/contact-us', '/partner-with-us', '/blogs', '/tnc'];
 
 export const authRoutes = ['/login', '/register', '/forgot-password'];
 
